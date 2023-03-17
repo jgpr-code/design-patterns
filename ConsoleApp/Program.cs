f1390dedcc98584eb22276140982dca7645609ff
Jörg Planner <joerg.planner@methodpark.de>
2023-03-17 14:01:00 +0100

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// In this simple C# Console App I want to run the examples from the other assemblies
