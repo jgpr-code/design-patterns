848545352cdac33916888f8d65a4599cc6c22d83
Jörg Planner <joerg.planner@methodpark.de>
2023-10-19 17:39:09 +0200

    behavioral, add visitor.cs

--- ObjectOrientedPatterns/Behavioral/Visitor.cs
+++ ObjectOrientedPatterns/Behavioral/Visitor.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ObjectOrientedPatterns.Behavioral
+﻿namespace ObjectOrientedPatterns.Behavioral.Visitor
 {
-    internal class Visitor
+    public abstract class IVisitor<TResult>
     {
+        public abstract TResult VisitLit(Lit lit);
+        public abstract TResult VisitPlus(PlusExp exp);
+        public abstract TResult VisitMinus(MinusExp exp);
+    }
+
+    public interface IExp
+    {
+        public TResult Accept<TResult>(IVisitor<TResult> visitor);
+    }
+
+    public class Lit : IExp
+    {
+        public TResult Accept<TResult>(IVisitor<TResult> visitor)
+        {
+            return visitor.VisitLit(this);
+        }
+        public int Value { get; set; }
+    }
+
+    public class PlusExp : IExp
+    {
+        public TResult Accept<TResult>(IVisitor<TResult> visitor)
+        {
+            return visitor.VisitPlus(this);
+        }
+        public IExp LeftExp { get; set; }
+        public IExp RightExp { get; set; }
+    }
+
+    public class MinusExp : IExp
+    {
+        public TResult Accept<TResult>(IVisitor<TResult> visitor)
+        {
+            return visitor.VisitMinus(this);
+        }
+        public IExp LeftExp { get; set; }
+        public IExp RightExp { get; set; }
     }
 }
